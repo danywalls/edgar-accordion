93dc0a7ae28c6147266a0cc8544f964dda577cee
Dany <dany@yukisoftware.es>
2020-01-15 09:38:38 +0100

    return the promise from fetch
    Removed not needed temp jokesResponse object.

--- src/component/norrisAPI.js
+++ src/component/norrisAPI.js
@@ -1,10 +1,8 @@
 const NORRIS_API = {
-    async getChuckNorris() {
-        const jokes = await fetch('https://api.chucknorris.io/jokes/random');
-        const jokesResponse = await jokes.json();
-        return jokesResponse;
-    }
-}
+  async getChuckNorris() {
+    const jokes = await fetch("https://api.chucknorris.io/jokes/random");
+    return await jokes.json();
+  }
+};
 
-
-export {NORRIS_API};
+export { NORRIS_API };
